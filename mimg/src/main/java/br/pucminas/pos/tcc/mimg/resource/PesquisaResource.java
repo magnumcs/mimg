598cde2f4c97324375fd1dd4157fb802533ace6f
Magnum Adelano <magnumcs@outlook.com>
2021-02-15 17:43:50 -0300

    MIMG - Primeiro commit

---
+++ mimg/src/main/java/br/pucminas/pos/tcc/mimg/resource/PesquisaResource.java
@@ -0,0 +1,27 @@
+package br.pucminas.pos.tcc.mimg.resource;
+
+import br.pucminas.pos.tcc.mimg.dto.PesquisaDTO;
+import br.pucminas.pos.tcc.mimg.exception.NoDataFoundException;
+import br.pucminas.pos.tcc.mimg.service.PesquisasService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/indicadores")
+public class PesquisaResource {
+
+    private final PesquisasService pesquisasService;
+
+    public PesquisaResource(PesquisasService pesquisasService) {
+        this.pesquisasService = pesquisasService;
+    }
+
+    @GetMapping(value = "/pesquisas/{type}")
+    ResponseEntity<PesquisaDTO> getPesquisasByType(@PathVariable(value = "type") Integer type) throws NoDataFoundException {
+        return ResponseEntity.ok(pesquisasService.getPesquisaByType(type));
+    }
+
+}
---
+++ mimg/src/main/java/br/pucminas/pos/tcc/mimg/util/MessagesUtil.java
@@ -0,0 +1,7 @@
+package br.pucminas.pos.tcc.mimg.util;
+
+public class MessagesUtil {
+
+    public static String PESQUISAS_NAO_ENCONTRADAS = "Não foram encontradas pesquisas relacionadas para esta consulta.";
+
+}
